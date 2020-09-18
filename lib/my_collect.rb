3a1c836626748cebec6e510e90b312f91bc0f742
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 01:28:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,15 @@
 def my_collect(array)
-  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    yield(array[i])
+    i += 1
   end
+  collection
+end
+
+
+my_collect(["Tim", "Tom", "Jim"]) do |name|
+  name.split(" ").first
 end
